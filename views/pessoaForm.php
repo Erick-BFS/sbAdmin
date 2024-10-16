bd1f73222abdb0321597c2e61f6caca8906014b4
Erick <126675963+Erick-BFS@users.noreply.github.com>
2024-10-16 14:20:54 -0300

    Update pessoaForm.php

--- views/pessoaForm.php
+++ views/pessoaForm.php
@@ -12,7 +12,7 @@
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Cadastro de Pessoa</h1>
                             </div>
-                            <form class="user" method="post" action="./pessoa.php?Action=insert">
+                            <form class="user" method="post" action="./pessoa.php?action=insert">
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="text" class="form-control form-control-user" name="nome" id="nome"
